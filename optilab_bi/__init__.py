eef1bfe5c486b09d6df7ce3b39e2edc2ec6cb860
Pamela Wai Lan <andre@gorillascode.com>
2018-04-13 12:43:06 -0300

    Conexão optilab

--- optilab_bi/__init__.py
+++ optilab_bi/__init__.py
@@ -5,8 +5,9 @@ import fdb
 db = SQLAlchemy()
 
 # engine = fdb.connect(dsn='D:\sgo\sgo.fdb', user='sysdba', password='ffracert', charset='latin1')
-engine = fdb.connect(dsn='D:\sgo\sgo.fdb', user='sysdba', password='masterkey', charset='latin1')
+# engine = fdb.connect(dsn='D:\sgo\sgo.fdb', user='sysdba', password='masterkey', charset='latin1')
 # engine = fdb.connect(dsn='192.168.10.12:sgo', user='sysdba', password='ffracert', charset='latin1')
+engine = fdb.connect(dsn='192.168.10.12:sgo', user='optilab', password='optilab', charset='latin1')
 
 db2 = engine.cursor()
 
