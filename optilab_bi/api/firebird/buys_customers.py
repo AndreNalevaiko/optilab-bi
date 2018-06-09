5d3ae3f246ea127120689c6586f9281b4931abbb
Andre <andre@gorillascode.com>
2018-06-09 17:19:51 -0300

    Quantidade de clientes ativos

--- optilab_bi/api/firebird/buys_customers.py
+++ optilab_bi/api/firebird/buys_customers.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from optilab_bi import connection
-from optilab_bi.api.firebird.sqls.buys_customer import eval_months_buys
+from optilab_bi.api.firebird.sqls.buys_customer import eval_months_buys, \
+active_current_previous_month, active_latest_year, active_today_yesterday
 from optilab_bi.api.mysql import configuration
 
 actions = Blueprint('/customers', __name__, url_prefix='/customers')
@@ -44,6 +45,42 @@ def build_result(brute_list):
 
     return result_final
 
+def calc_amonts(result_day, result_month, result_lates_year, filters):
+    result = {
+        'current_day': [],
+        'latest_day': [],
+        'current_month': [],
+        'latest_month': [],
+        'average_latest_year': []
+    }
+
+    for row in result_day:
+        obj = {'qtd': row[0], 'emp_code': row[1]}
+        if row[2] == int(filters['current_day']):
+            result['current_day'].append(obj)
+        else:
+            result['latest_day'].append(obj)
+            
+    for row in result_month:
+        obj = {'qtd': row[0], 'emp_code': row[1]}
+        if row[2] == int(filters['current_month']):
+            result['current_month'].append(obj)
+        else:
+            result['latest_month'].append(obj)
+
+    emp_codes = list(set([r[1] for r in result_lates_year]))
+    import ipdb; ipdb.set_trace()
+    for emp_code in emp_codes:
+        list_emp = [r[0] for r in result_lates_year if r[1] == emp_code]
+        average = sum(list_emp)/len(list_emp)
+
+        obj = {'average': average, 'emp_code': emp_code}
+        result['average_latest_year'].append(obj)
+
+    return result
+        
+
+
 @actions.route('/_eval', methods=['POST'])
 def _eval():
     session = connection.cursor()
@@ -83,6 +120,67 @@ def _eval():
 
         result_list.append(rate)
     
-    response = jsonify(build_result(result_list))
+    response = build_result(result_list)
+    
+    for item in response:
+        if not item['current_value']:
+            item['variation'] = 0
+            item['current_value'] = 0        
+        elif not item['latest_value']:
+            item['variation'] = 0
+            item['latest_value'] = 0                    
+        else:
+            item['variation'] = (item['current_value'] / item['latest_value']) - 1
 
-    return response
+    return jsonify(response)
+
+
+@actions.route('/_amount', methods=['POST'])
+def _amount():
+    session = connection.cursor()
+
+    sql_month = active_current_previous_month()
+    sql_day = active_today_yesterday()
+    sql_latest_year = active_latest_year()
+
+    args = request.get_json()
+
+    list_cfop = configuration.get_config('cfop_vendas')
+
+    current_year = args.get('year')
+    latest_year = str(int(current_year) - 1)
+    current_month = args.get('month')
+    latest_month = str(int(current_month) - 1)
+    current_day = args.get('day')
+    latest_day = str(int(current_day) - 1)
+
+    sql_month = sql_month.format(list_cfop=list_cfop, current_year=current_year,\
+                            current_month=current_month, latest_month=latest_month)
+
+    sql_day = sql_day.format(list_cfop=list_cfop, current_day=current_day, latest_day=latest_day, \
+                            current_month=current_month, current_year=current_year)
+
+    sql_latest_year = sql_latest_year.format(list_cfop=list_cfop, latest_year=latest_year)
+
+    session.execute(sql_month)
+    result_month = session.fetchall()
+
+    session.execute(sql_day)
+    result_day = session.fetchall()
+
+    session.execute(sql_latest_year)
+    result_latest_year = session.fetchall()
+
+    filters = {
+        'current_year': current_year,
+        'latest_year': latest_year,
+        'current_month': current_month,
+        'latest_month': latest_month,
+        'current_day': current_day,
+        'latest_day': latest_day,
+    }
+
+    result = calc_amonts(result_day, result_month, result_latest_year, filters)
+
+    return jsonify(result)
+    
--- optilab_bi/api/firebird/sqls/buys_customer.py
+++ optilab_bi/api/firebird/sqls/buys_customer.py
@@ -7,22 +7,23 @@ def eval_months_buys():
     left join clien cli on cli.CLICODIGO = nfs.CLICODIGO 
     left join nfpro    nfp on nfs.nfcodigo  = nfp.nfcodigo
                             and nfs.empcodigo = nfp.empcodigo
-    where   EXTRACT(MONTH FROM nfs.nfdtemis) IN ({latest_month},{current_month}) 
+    where  EXTRACT(MONTH FROM nfs.nfdtemis) IN ({latest_month},{current_month}) 
     and EXTRACT(YEAR FROM nfs.nfdtemis) in ({years}) and nfs.nfsit ='N'
     and nfs.fiscodigo1 in ({list_cfop})
     group by emp_code, num_month, CLICODIGO, NOMEFANT, num_year
     """
 
-# TODO alterar para medir os ativos pelos pedidos
 def active_today_yesterday():
     return """
     /*DIA ATUAL E DIA ANTERIOR*/
-    select COUNT(distinct nfs.CLICODIGO), iif( cli.funcodigo = 858, 5, nfs.empcodigo ) emp_code, EXTRACT(DAY FROM nfs.nfdtemis) day_buy
-    from notas nfs
-    left join clien cli on cli.CLICODIGO = nfs.CLICODIGO
-    where nfs.NFDTEMIS in ('02/02/2018', '02/01/2018')
-    and nfs.nfsit ='N'
-    and nfs.fiscodigo1 in ('5.101','5.102','5.116','6.116','6.101','6.102','5.124','5.112')
+    select COUNT(distinct ped.CLICODIGO), iif( cli.funcodigo = 858, 5, ped.empcodigo ) emp_code, EXTRACT(DAY FROM ped.PEDDTEMIS) day_buy
+    from pedid ped
+    left join clien cli on cli.CLICODIGO = ped.CLICODIGO
+    where 
+    EXTRACT(DAY FROM ped.PEDDTEMIS) IN ({current_day},{latest_day}) AND 
+    EXTRACT(YEAR FROM ped.PEDDTEMIS) = {current_year}  AND
+    EXTRACT(MONTH FROM ped.PEDDTEMIS) = {current_month}
+    and (ped.FISCODIGO1 in ({list_cfop}) or ped.FISCODIGO2 in ({list_cfop}))
     group by emp_code, day_buy
     order by day_buy asc, emp_code asc
     """
@@ -30,25 +31,26 @@ def active_today_yesterday():
 def active_current_previous_month():
     return """
     /*MES ATUAL E MES ANTERIOR*/
-    select COUNT(distinct nfs.CLICODIGO), iif( cli.funcodigo = 858, 5, nfs.empcodigo ) emp_code, EXTRACT(MONTH FROM nfs.nfdtemis) num_month
-    from notas nfs
-    left join clien cli on cli.CLICODIGO = nfs.CLICODIGO
-    where   EXTRACT(MONTH FROM nfs.nfdtemis) IN ('02','03') and EXTRACT(YEAR FROM nfs.nfdtemis) in ('2018')
-    and nfs.nfsit ='N'
-    and nfs.fiscodigo1 in ('5.101','5.102','5.116','6.116','6.101','6.102','5.124','5.112')
+    select COUNT(distinct ped.CLICODIGO), iif( cli.funcodigo = 858, 5, ped.empcodigo ) emp_code, EXTRACT(MONTH FROM ped.PEDDTEMIS) num_month
+    from pedid ped
+    left join clien cli on cli.CLICODIGO = ped.CLICODIGO
+    where 
+    EXTRACT(YEAR FROM ped.PEDDTEMIS) = {current_year}  AND
+    EXTRACT(MONTH FROM ped.PEDDTEMIS) in ({current_month}, {latest_month})
+    and (ped.FISCODIGO1 in ({list_cfop}) or ped.FISCODIGO2 in ({list_cfop}))
     group by emp_code, num_month
     order by num_month asc, emp_code asc
     """
 
-def activer_latest_year():
+def active_latest_year():
     return """
     /*MEDIA ANO ANTERIOR - TEM QUE CALCULAR NO BACKEND*/
-    select COUNT(distinct nfs.CLICODIGO), iif( cli.funcodigo = 858, 5, nfs.empcodigo ) emp_code, EXTRACT(MONTH FROM nfs.nfdtemis) num_month
-    from notas nfs
-    left join clien cli on cli.CLICODIGO = nfs.CLICODIGO
-    where EXTRACT(YEAR FROM nfs.nfdtemis) = '2018'
-    and nfs.nfsit ='N'
-    and nfs.fiscodigo1 in ('5.101','5.102','5.116','6.116','6.101','6.102','5.124','5.112')
+    select COUNT(distinct ped.CLICODIGO), iif( cli.funcodigo = 858, 5, ped.empcodigo ) emp_code, EXTRACT(MONTH FROM ped.PEDDTEMIS) num_month
+    from pedid ped
+    left join clien cli on cli.CLICODIGO = ped.CLICODIGO
+    where 
+    EXTRACT(YEAR FROM ped.PEDDTEMIS) = {latest_year}
+    and (ped.FISCODIGO1 in ({list_cfop}) or ped.FISCODIGO2 in ({list_cfop}))
     group by emp_code, num_month
     order by num_month asc, emp_code asc
     """
