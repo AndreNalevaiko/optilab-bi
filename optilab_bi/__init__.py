d6de7b6037951652e2e828a9b20ae27be503bf85
Andre <andre@gorillascode.com>
2019-07-01 23:19:48 -0300

    Fix e ajustes

--- optilab_bi/__init__.py
+++ optilab_bi/__init__.py
@@ -5,8 +5,12 @@ from flask_user import FlaskUser
 import fdb
 
 db = SQLAlchemy()
-db_metrics = create_engine(config.DATABASE_METRICS)
+db_metrics = create_engine(config.DATABASE_METRICS, pool_recycle=1800)
 user_manager = FlaskUser()
 
 def get_connection():
-    return fdb.connect(dsn='/home/andre/Documentos/databases/sgo.fdb', user='sysdba', password='masterkey', charset='latin1')
+    return fdb.connect(
+        dsn=config.DATABASE_SGO_URL,
+        user=config.DATABASE_SGO_USER,
+        password=config.DATABASE_SGO_PASS,
+        charset='latin1')
