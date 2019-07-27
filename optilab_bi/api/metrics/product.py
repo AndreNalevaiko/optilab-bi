feb7387e5190c2052a74fad14092bad3fa36682c
Andre <andre@gorillascode.com>
2019-07-27 14:18:19 -0300

    Ajuste relatorio produtos

--- optilab_bi/api/metrics/product.py
+++ optilab_bi/api/metrics/product.py
@@ -8,6 +8,56 @@ from flask import Blueprint, jsonify, request
 
 actions = Blueprint('/product', __name__, url_prefix='/product')
 
+select_products_by_wallet = """
+    SELECT
+    c.product,
+    c.product_group,
+    c.wallet wallet,
+    sum(IF(year(c.date) = {last_year}, c.sold_amount, 0 )) / 12 avg_month_qtd_last_year,
+    sum(IF(year(c.date) = {last_year}, c.sold_value, 0 )) / 12 avg_month_value_last_year,
+    sum(IF(year(c.date) = {current_year} and month(c.date) <= {last_month}, c.sold_amount, 0 )) / {last_month} avg_month_qtd_current_year,
+    sum(IF(year(c.date) = {current_year} and month(c.date) <= {last_month}, c.sold_value, 0 )) / {last_month} avg_month_value_current_year,
+    sum(IF(year(c.date) = {current_year} and month(c.date) = {current_month} and day(c.date) <= {current_day}, c.sold_amount, 0 )) qtd_current_month,
+    sum(IF(year(c.date) = {current_year} and month(c.date) = {current_month} and day(c.date) <= {current_day}, c.sold_value, 0 )) value_current_month
+    FROM consolidation c
+    WHERE date BETWEEN '{init_date}' AND '{end_date}'
+    group by product, product_group, wallet
+    """
+
+
+select_products_global = """
+    SELECT
+    c.product,
+    c.product_group,
+    'Global' _wallet,
+    sum(IF(year(c.date) = {last_year}, c.sold_amount, 0 )) / 12 avg_month_qtd_last_year,
+    sum(IF(year(c.date) = {last_year}, c.sold_value, 0 )) / 12 avg_month_value_last_year,
+    sum(IF(year(c.date) = {current_year} and month(c.date) <= {last_month}, c.sold_amount, 0 )) / {last_month} avg_month_qtd_current_year,
+    sum(IF(year(c.date) = {current_year} and month(c.date) <= {last_month}, c.sold_value, 0 )) / {last_month} avg_month_value_current_year,
+    sum(IF(year(c.date) = {current_year} and month(c.date) = {current_month} and day(c.date) <= {current_day}, c.sold_amount, 0 )) qtd_current_month,
+    sum(IF(year(c.date) = {current_year} and month(c.date) = {current_month} and day(c.date) <= {current_day}, c.sold_value, 0 )) value_current_month
+    FROM consolidation c
+    WHERE date BETWEEN '{init_date}' AND '{end_date}'
+    group by product, product_group, _wallet
+    """
+
+select_products_others_wallet = """
+    SELECT
+    c.product,
+    c.product_group,
+    'Others' _wallet,
+    sum(IF(year(c.date) = {last_year}, c.sold_amount, 0 )) / 12 avg_month_qtd_last_year,
+    sum(IF(year(c.date) = {last_year}, c.sold_value, 0 )) / 12 avg_month_value_last_year,
+    sum(IF(year(c.date) = {current_year} and month(c.date) <= {last_month}, c.sold_amount, 0 )) / {last_month} avg_month_qtd_current_year,
+    sum(IF(year(c.date) = {current_year} and month(c.date) <= {last_month}, c.sold_value, 0 )) / {last_month} avg_month_value_current_year,
+    sum(IF(year(c.date) = {current_year} and month(c.date) = {current_month} and day(c.date) <= {current_day}, c.sold_amount, 0 )) qtd_current_month,
+    sum(IF(year(c.date) = {current_year} and month(c.date) = {current_month} and day(c.date) <= {current_day}, c.sold_value, 0 )) value_current_month
+    FROM consolidation c
+    WHERE date BETWEEN '{init_date}' AND '{end_date}'
+    AND wallet not in ({wallets})
+    group by product, product_group, _wallet
+    """
+
 def consolidate_result(result):
     return_ = []
 
@@ -16,15 +66,20 @@ def consolidate_result(result):
         obj = {}
 
         for key in keys:
+            key_name = key
+
+            # TODO Fix para group by funcionar corretamente
+            if key[0] == '_':
+                key_name = key.replace('_', '', 1)
+
             if isinstance(row[key], decimal.Decimal):
-                obj[key] = str(row[key])
+                obj[key_name] = str(row[key])
             else:
-                obj[key] = row[key]
+                obj[key_name] = row[key]
         
         return_.append(obj)
 
     return return_
-
 
 @actions.route('/billings', methods=['POST'])
 @user_manager.auth_required('user')
@@ -34,6 +89,7 @@ def products(auth_data=None):
     with db_metrics.connect() as con:
         customer = params.get('customer')
         date = params.get('date')
+        wallets = params.get('wallets')
         date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%fZ")
 
         current_year = date.year
@@ -48,25 +104,13 @@ def products(auth_data=None):
         init_date = date.replace(day=1, month=1, year=last_year).strftime('%Y-%m-%d')
         end_date = date.replace(day=current_day, month=current_month, year=current_year).strftime('%Y-%m-%d')
 
-        sql = """
-        SELECT
-        c.product,
-        c.product_group,
-        c.wallet,
-        sum(IF(year(c.date) = {last_year}, c.sold_amount, 0 )) / 12 avg_month_qtd_last_year,
-        sum(IF(year(c.date) = {last_year}, c.sold_value, 0 )) / 12 avg_month_value_last_year,
-        sum(IF(year(c.date) = {current_year} and month(c.date) <= {last_month}, c.sold_amount, 0 )) / {last_month} avg_month_qtd_current_year,
-        sum(IF(year(c.date) = {current_year} and month(c.date) <= {last_month}, c.sold_value, 0 )) / {last_month} avg_month_value_current_year,
-        sum(IF(year(c.date) = {current_year} and month(c.date) = {current_month} and day(c.date) <= {current_day}, c.sold_amount, 0 )) qtd_current_month,
-        sum(IF(year(c.date) = {current_year} and month(c.date) = {current_month} and day(c.date) <= {current_day}, c.sold_value, 0 )) value_current_month
-        FROM consolidation c
-        WHERE date BETWEEN '{init_date}' AND '{end_date}'
-        group by product, product_group, wallet
-        """
+        wallets = ",".join(wallets)
 
+        response = []
 
-        # for column in metrics_columns:
-        sql_ = sql.format(
+        sqls_ = [select_products_by_wallet, select_products_global, select_products_others_wallet]
+        for sql_ in sqls_:
+            sql = sql_.format(
             current_year=current_year,
             last_year=last_year,
             current_month=current_month,
@@ -75,8 +119,11 @@ def products(auth_data=None):
             customer=customer,
             init_date=init_date,
             end_date=end_date,
-        )
+            wallets=wallets
+            )
 
-        result = con.execute(sql_)
+            result = con.execute(sql)
+            response = response + consolidate_result(result)
 
-    return jsonify(consolidate_result(result))
+
+    return jsonify(response)
